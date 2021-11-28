4d299fa3e0597a4bf8720e80ff78ae8301a0f7e9
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2021-11-28 18:18:24 +0100

    Eingabeverarbeitung Fallunterscheidung tests

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+
+
+
+
+"""Input
+
+alle Zeilen mit = in einen Array
+alle Zeile mit : in einen Array
+
+"""
+nodeEingabeString = []
+eingabeGewichteString = []
+
 eingabe = open('Input-File', 'r')
 
-
 for zeile in eingabe:
-	print("Inhalt aus Datei: ")
-	print(zeile)
+	if(0 == zeile.__contains__("//")):
+		if(zeile.__contains__("=")):
+			nodeEingabeString.append(zeile)
+		elif(zeile.__contains__("-") and zeile.__contains__(":")):
+			eingabeGewichteString.append(zeile)
 
 eingabe.close()
+
+for zeile in nodeEingabeString:
+	print(zeile)
+
+
+for zeile in eingabeGewichteString:
+	print(zeile)
