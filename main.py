2026f400bcbb123c13323f1d76e8e7386dae4443
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2021-11-28 18:37:28 +0100

    Eingabeverarbeitung Fallunterscheidung tests

--- main.py
+++ main.py
@@ -24,7 +24,23 @@ eingabe.close()
 
 for zeile in nodeEingabeString:
 	print(zeile)
+	zeile = zeile.replace(' ', "").replace(';','').replace('\n','')
+	ortZeichenIstgleich = zeile.find("=")
+	nodeName = zeile[0:ortZeichenIstgleich]
+	print("NodeName:---" + nodeName + "---")
+	nodeId = zeile[ortZeichenIstgleich+1:]
+	print("NodeId:---" + nodeId + "---")
+
 
 
 for zeile in eingabeGewichteString:
 	print(zeile)
+	zeile = zeile.replace(' ', "").replace(';', '').replace('\n', '')
+	ortZeichenBindestrich = zeile.find("-")
+	ortZeichenDoppelPunkt = zeile.find(":")
+	node1 = zeile[0:ortZeichenBindestrich]
+	print("Node1:---" + node1 + "---")
+	node2 = zeile[ortZeichenBindestrich+1:ortZeichenDoppelPunkt]
+	print("Node2:---" + node2 + "---")
+	kantenGewicht = zeile[ortZeichenDoppelPunkt:]
+	print("Kantengewicht:---" + kantenGewicht + "---")
