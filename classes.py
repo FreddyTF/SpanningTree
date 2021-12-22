672a170426f8a8045a36e09baafaa7f667c44d17
Frederic <frederic.tabo.findeis@gmx.de>
2021-12-22 16:01:29 +0100

    angefangen nachrichtenaustausch implementierung

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ class Node:
         self.name = name            # Bezeichner des Knotens
         self.nodeID = nodeID        # Knoten ID > 0
         self.link = []              # Liste aller pot.Nachbarknoten
-        self.nextHop = self.name    # Berechneter Link zum nächsten Knoten in Richtung Root
+        self.nextHop = 0            # Berechneter Link zum nächsten Knoten in Richtung Root
         self.msgCnt = 0             # Zählt mit, wie oft der Knoten bei der Bearbeitung des Algorithmus aufgerufen wird
 
 class Link:
@@ -20,3 +20,21 @@ class Link:
     def __repr__(self):
         string = str(self.kosten)
         return string.rjust(3)
+
+def nachrichtenaustausch(nodes, links):
+    for node in nodes:
+        sendeZeileIndex = nodes.index(node)
+        zeile = links[sendeZeileIndex]
+        listeKanten = []
+        for i in range(len(zeile)):
+            if zeile[i].kosten > 0:
+               listeKanten.append(i)
+
+        for i in listeKanten:
+            if node.nodeID < links[i][sendeZeileIndex].rootID:
+                links[i][sendeZeileIndex].rootID = node.nodeID
+                links[i][sendeZeileIndex].summeKosten = node.nextHop
+            else if node.nodeID <= links[i][sendeZeileIndex].rootID:
+                if (node.nextHop + links[i][sendeZeileIndex].kosten) < links[i][sendeZeileIndex].summeKosten:
+                    links[i][sendeZeileIndex].rootID = node.nodeID
+                    links[i][sendeZeileIndex].summeKosten = node.nextHop
